fdcf4fc7d82411a3f76bc00697f521071ff5f86f
Esther Aires <estherlourenco777@gmail.com>
2025-02-13 15:39:32 -0300

    Operadores

---
+++ dia6/script.js
@@ -0,0 +1,31 @@
+/////OPERADORES ARITMÉTICOS SIMPLES/////
+
+// let total = 6 + 6 // adição
+// total = 6 - 2 // subtração
+// total = 6 * 2 // multiplicação
+// total = 6 / 2 // divisão
+
+// let n1 = 6
+// let n2 = 2
+// let total = n1 + n2
+
+// console.log(total)
+
+/////OPERADORES ARITMÉTICOS COMPLEXOS/////
+// let total = 10 % 3 // resto da divisão
+// // total++ // incremento
+// // total-- // decremento
+// // total = 5 ** 3 // exponenciação
+
+// console.log(total)
+
+/////OPERADORES DE ATRINUIÇÃO/////
+
+let total = 10
+// total += 5 //adicionar +5
+// total -= 5 //subtração -5
+// total *= 5 //multiplicação -5
+// total /= 5 //divisão
+total %= 4 //resto
+total **= 2 //exponenciação
+console.log(total)
