4661cdff1bb2917e3f5018bd68180b4ef9d1f885
Esther Aires <estherlourenco777@gmail.com>
2025-02-13 09:39:16 -0300

    variáveis

---
+++ dia3/script.js
@@ -0,0 +1,19 @@
+//Variaveis: armazenar dados momentaneamente
+//-var-deixou de ser utilizada
+// let const
+
+// A variavel deve ser inicializada antes de ser utilizada
+
+//Let é possível alterar ou reassociar um novo valor ou manipular o valor da variável
+
+let firstName = 'Andre'
+let lastName = 'Iacono'
+let age = 25
+console.log(firstName, lastName, age)
+
+//Const, não é possível fazer a manipulação de dados
+//Usar const para valores que sabemos que não mudarão,
+// como URLs de API, valores de configuração
+
+const pi = 3.1415
+console.log(p1)
