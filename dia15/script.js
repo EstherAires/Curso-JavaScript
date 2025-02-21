17522560d2b01250ce89edca8bad164a15b3ae30
Esther Aires <estherlourenco777@gmail.com>
2025-02-21 10:55:13 -0300

    NOT Operator

---
+++ dia15/script.js
@@ -0,0 +1,26 @@
+//IF ELSE com NOT Operator
+
+// let usuariologado = false
+
+// if (!usuariologado) {
+//   console.log(`Por gentileza realize o login`)
+// }
+
+//NOT Operator VERIFICANDO OS ITENS DE UMA LISTA
+
+// let listaProdutos = []
+
+// if (!listaProdutos.length) {
+//   console.log(`A lista ta vazia`)
+// } else {
+// }
+
+/*Crie uma solução que verifica se o usuário escolheu a cor azul.
+Caso seja outra cor, utlize o NOT operator para retornar a mensagem
+"Não temos essa cor"*/
+
+let corEscolhida = 'Branco'
+
+if (!(corEscolhida == 'Azul')) {
+  console.log(`Não temos essa cor`)
+}
